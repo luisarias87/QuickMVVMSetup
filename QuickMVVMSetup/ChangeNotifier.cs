74ae3925682206450463d61f70a1d061396bb98f
luisarias87 <ariasluisla89@gmail.com>
2022-11-03 11:35:37 -0500

    Add project files.

---
+++ QuickMVVMSetup/ChangeNotifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+
+namespace QuickMVVMSetup
+{
+    public class ChangeNotifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ QuickMVVMSetup/MainVM.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+
+namespace QuickMVVMSetup
+{
+    public class MainVM : ChangeNotifier
+    {
+        
+
+        public MainVM()
+        {
+            
+            Persons = new ObservableCollection<Person>();
+        }
+
+        private Person person;
+
+        public Person TargetPerson
+        {
+            get { return person; }
+            set { person = value; OnPropertyChanged(nameof(TargetPerson)); }
+        }
+
+        private ObservableCollection<Person> _persons;
+
+        public ObservableCollection<Person> Persons
+        {
+            get { return _persons; }
+            set { _persons = value;  }
+        }
+
+        
+
+        public void AddPerson(object person1) 
+        {
+            Persons.Add(TargetPerson);// add it to the command 
+
+            TargetPerson = new Person();
+        }
+
+
+        public ICommand CMDAdd => new RelayCommand(AddPerson, null);
+
+
+
+
+
+
+
+    }
+}
